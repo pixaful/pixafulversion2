4c876ad681fabd0baecefb7646835ce2c26767cb
Hayden <hello@haydentomas.com>
2019-08-21 22:09:56 +0100

    template parts

--- site/static/js/pixaful.js
+++ site/static/js/pixaful.js
@@ -114,37 +114,3 @@ var sticky = new Sticky('.sticky');
 sticky.destroy();
 
 
-var stage = new createjs.Stage("blogcanvas");
-
-var c = new createjs.Shape();
-c.graphics.f("#f00").dc(0, 0, 50); // Drawn a 100x100 circle from the center
-
-var t = new createjs.Text("Resize the browser/frame to redraw", "24px Arial bold", "#000");
-t.x = t.y = 20;
-stage.addChild(c, t);
-
-window.addEventListener("resize", handleResize);
-function handleResize() {
-  var w = window.innerWidth - 2; // -2 accounts for the border
-  var h = window.innerHeight - 2;
-  stage.canvas.width = w;
-  stage.canvas.height = h;
-  //
-  var ratio = 100 / 100; // 100 is the width and height of the circle content.
-  var windowRatio = w / h;
-  var scale = w / 100;
-  if (windowRatio > ratio) {
-    scale = h / 100;
-  }
-  // Scale up to fit width or height
-  c.scaleX = c.scaleY = scale;
-
-  // Center the shape
-  c.x = w / 2;
-  c.y = h / 2;
-
-  stage.update();
-}
-
-handleResize(); // First draw
-
