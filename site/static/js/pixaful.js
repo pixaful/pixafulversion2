02131ef83fd6cc7845f97efdd2467ac349f3004f
hayden Tomas <hello@haydentomas.com>
2019-10-24 08:44:53 +0100

    less preload time

--- site/static/js/pixaful.js
+++ site/static/js/pixaful.js
@@ -112,7 +112,7 @@ var sticky = new Sticky('.sticky');
 sticky.destroy();
 $(window).on('load', function () { // makes sure the whole site is loaded 
   $('#status').fadeOut(); // will first fade out the loading animation 
-  $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
-  $('body').delay(350).css({ 'overflow': 'visible' });
+  $('#preloader').delay(50).fadeOut('slow'); // will fade out the white DIV that covers the website. 
+  $('body').delay(50).css({ 'overflow': 'visible' });
 })
  
