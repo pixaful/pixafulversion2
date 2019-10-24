ffcd648866f9f644410e1e1c4e1e01bdb73a3ea5
hayden Tomas <hello@haydentomas.com>
2019-10-24 08:54:16 +0100

    diff delay

--- site/static/js/pixaful.js
+++ site/static/js/pixaful.js
@@ -112,7 +112,7 @@ var sticky = new Sticky('.sticky');
 sticky.destroy();
 $(window).on('load', function () { // makes sure the whole site is loaded 
   $('#status').fadeOut(); // will first fade out the loading animation 
-  $('#preloader').delay(50).fadeOut('slow'); // will fade out the white DIV that covers the website. 
-  $('body').delay(50).css({ 'overflow': 'visible' });
+  $('#preloader').delay(150).fadeOut('slow'); // will fade out the white DIV that covers the website. 
+  $('body').delay(150).css({ 'overflow': 'visible' });
 })
  
