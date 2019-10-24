3f66be04d41d3e8688fbc15a64846c84b7270a84
hayden Tomas <hello@haydentomas.com>
2019-10-24 08:20:01 +0100

    js 1

--- site/static/js/pixaful.js
+++ site/static/js/pixaful.js
@@ -110,9 +110,4 @@ if ($('#back-to-top').length) {
 }
 var sticky = new Sticky('.sticky');
 sticky.destroy();
-$(window).on('load', function () { // makes sure the whole site is loaded 
-  $('#status').fadeOut(); // will first fade out the loading animation 
-  $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
-  $('body').delay(350).css({ 'overflow': 'visible' });
-})
-
+ 
